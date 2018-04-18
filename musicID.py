feadeb8afc1875a33a255db9436815536afd7c24
there-is-no-sp00n <22650592+there-is-no-sp00n@users.noreply.github.com>
2018-04-18 12:22:26 -0500

    Add files via upload

--- musicID.py
+++ musicID.py
@@ -1,51 +1,41 @@
+#Aninda Aminuzzaman
+
+#1001018367
+
+
 from scipy import signal
-
 import scipy.io.wavfile
-
 import glob
-
 import numpy as np
 
 song_db = glob.glob('song-*.wav');
 
-print(song_db, len(song_db))
+test_song = scipy.io.wavfile.read('testSong.wav')
 
-#for i in song_db:
-	
-
-b = scipy.io.wavfile.read('song-winds.wav')
-
-print(b)
-
-x = b[1]
-fs = b[0]
-
+x = test_song[1]
+fs = test_song[0]
 f, t, Sxx = signal.spectrogram(x, fs=fs, nperseg=0.5*fs)
 
-print(f, len(f))
+freq = Sxx.argmax(axis=0)
 
-print(t, len(t))
+for j in range(0, len(freq)):
+	freq[j] = f[freq[j]]
 
-print(Sxx, len(Sxx[0]))
+song_norms = {}
 
+for i in song_db:
+	b = scipy.io.wavfile.read(i)
+	x = b[1]
+	fs = b[0]
+	f, t, Sxx = signal.spectrogram(x, fs=fs, nperseg=0.5*fs)
 
-temp = 0
-final = 0
-arr = []
+	freq_ind = Sxx.argmax(axis=0)
+	for j in range(0, len(freq_ind)):
+		freq_ind[j] = f[freq_ind[j]]
+	norm = np.linalg.norm(freq_ind - freq, ord=1)
+	song_norms[i] = norm
 
-d = Sxx.argmax(axis=0)
+fin = sorted(song_norms.items(), key=lambda x: x[1])
 
-print(d, len(d))
-
-for i in range(0, len(d)):
-	d[i] = f[d[i]]
-
-print(d, len(d))
-
-total = 0
-for i in d:
-	total = total + i
-
-print(total)
-
-print(np.linalg.norm(d, ord=1))
+for i in fin:
+	print(i[1], "  ", i[0])
